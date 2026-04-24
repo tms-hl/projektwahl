be29418b30792a3b10fac4a4106f0b972fa73793
Daniel <daniel.klingebiel@gmail.com>
2026-04-24 22:58:52 +0200

    OAuth

--- model.py
+++ model.py
@@ -9,15 +9,16 @@ class Raum:
     
             
 class Person:
-    def __init__(self, uid, email, vorname, nachname):
+    def __init__(self, uid=None, email=None, vorname=None, nachname=None):
         self.uid = uid
         self.email = email
         self.vorname = vorname
         self.nachname = nachname
 
 class Schueler(Person):
-    def __init__(self, klasse, wahl):
-        self.klasse = klasse
+    def __init__(self):
+        super().__init__()
+        self.klasse = None
 
 class Projekt:
     def __init__(self):
@@ -56,17 +57,22 @@ class Db:
     @contextmanager
     def get_cursor(self):
         """Erstellt einen Cursor für den Kontext-Manager"""
-        conn = self.get_connection()
+        conn = self.pool.get_connection()
         cursor = conn.cursor(dictionary=True)
         try:
             yield cursor
         finally:
             cursor.close()
             conn.close()  # Verbindung zurück in den Pool geben
-            
+    
+    @contextmanager
     def get_connection(self):
         """Methode, um eine Verbindung aus dem Pool zu erhalten"""
-        return self.pool.get_connection()
+        conn = self.pool.get_connection()
+        try:
+            yield conn
+        finally:
+            conn.close()
     
     def get_project(self, pid):
         
@@ -86,9 +92,16 @@ class Db:
                 p.klasse_min = result[0]['klasse_min']
                 p.klasse_max = result[0]['klasse_max']
                 
-                # TODO: Raum mit hinzufügen
-                # TODO: Orginator(en) hinzufügen
-                # TODO: Teilnehmer hinzufügen
+                query = "SELECT user.uid, email, lastname, firstname FROM user JOIN leitet ON leitet.uid = user.uid WHERE leitet.pid = %s"
+                result = cursor.execute(query, [p.pid])
+                p.organisatoren = [
+                    Person(
+                        row['uid'],
+                        row['email'],
+                        row['firstname'],
+                        row['lastname']
+                    ) for row in cursor.fetchall()
+                ]
                 
                 return p
 
@@ -102,10 +115,8 @@ class Db:
         with self.get_cursor() as cursor:  # Verbindung im 'with'-Block
             query = "SELECT * FROM projekt"
             cursor.execute(query)
-            result = cursor.fetchall()
-            
             projects = []
-            for row in result:
+            for row in cursor.fetchall():
                 p = Projekt()
                 p.pid = row ['pid']
                 p.name = row ['name']
@@ -126,13 +137,28 @@ class Db:
             Mats
         '''
         with self.get_cursor() as cursor:  # Verbindung im 'with'-Block
-            query = "SELECT * JOIN FROM wählt WHERE uid = %s"
+            query = "SELECT * FROM wählt JOIN projekt ON projekt.pid = wählt.pid WHERE uid = %s"
             cursor.execute(query, [uid])
-            result = cursor.fetchall()
+            projects = []
+            for row in cursor.fetchall():
+                p = Projekt()
+                p.pid = row ['pid']
+                p.name = row ['name']
+                p.beschreibung = row['beschreibung']
+                p.plaetze_min = row['plätze_min']
+                p.plaetze_max = row['plätze_max']
+                p.klasse_min = row['klasse_min']
+                p.klasse_max = row['klasse_max']
+                projects.append(p)
+                
+        return projects
+
+    def has_choice(self, uid):
+        with self.get_cursor() as cursor:
+            query = "SELECT 1 FROM wählt WHERE uid = %s LIMIT 1"
+            cursor.execute(query, [uid])
+            return cursor.fetchone() is not None
             
-            w = Wahl()
-            return result
-        
     def get_user(self, uid):
         '''
             Gibt den Schüler mit der ID uid zurück
@@ -141,23 +167,74 @@ class Db:
             Louis
         '''
         with self.get_cursor() as cursor:
-            query = "SELECT * FROM `user` WHERE uid = %s"
+            query = "SELECT * FROM `user` WHERE uid = %s LIMIT 1"
             cursor.execute(query, [uid])
-            result = cursor.fetchall()
+            result = cursor.fetchone()
 
             if result is None:
                 return None
             else:
-                u = Person(
-                    result[0]['uid'],
-                    result[0]['email'],
-                    result[0]['firstname'],
-                    result[0]['lastname']
-                )
-
-                return u
-                
+                return self.add_user_info(result)
     
+    def add_user_info(self, result):
+        uid = result['uid']
+        with self.get_cursor() as cursor:
+            query = 'SELECT 1 FROM gruppenmitglied JOIN gruppe ON gruppe.gid = gruppenmitglied.gId WHERE gruppenmitglied.uId = %s AND gruppe.account = "lehrer" LIMIT 1'
+            cursor.execute(query, [uid])
+            if cursor.fetchone() is None:       # Schüler
+                query = "SELECT name FROM klasse JOIN klassenmitglied ON klassenmitglied.cId = klasse.cId WHERE uId = %s LIMIT 1"
+                cursor.execute(query, [uid])
+                row = cursor.fetchone()
+                p = Schueler()
+                p.klasse = row['name']
+            else:                               # Lehrer
+                p = Organisator()
+
+            p.uid = result['uid']
+            p.email = result['email']
+            p.vorname = result['firstname']
+            p.nachname = result['lastname']
+            
+            return p
+        
+    def get_user_by_email(self, email):
+        '''
+            Gibt den Schüler mit der email zurück
+            
+            Author:
+            Louis
+        '''
+        with self.get_cursor() as cursor:
+            query = "SELECT * FROM `user` WHERE email = %s LIMIT 1"
+            cursor.execute(query, [email])
+            result = cursor.fetchone()
+
+            if result is None:
+                return None
+            else:
+                return self.add_user_info(result)
+                
+    def get_users(self, uid_list=None):
+        '''
+            Gibt eine Liste von Benutzern zurück. Wenn uid_list eine Liste (nicht None) ist, werden nur die Nutzer mit den uids der Liste zurückgegeben
+        '''
+        query = "SELECT uid, email, firstname, lastname FROM user"
+        
+        if uid_list is not None:
+            id_str = ",".join([int(uid) for uid in uid_list])
+            query += " WHERE uid IN ({id_str})"
+        
+        with self.get_cursor() as cursor:
+            cursor.execute(query)
+            return [
+                Person(
+                    row['uid'],
+                    row['email'],
+                    row['firstname'],
+                    row['lastname']
+                ) for row in cursor.fetchall()
+            ]
+                
     def get_rooms(self):
         '''
             Gibt eine Liste mit allen Räumen zurück
@@ -165,7 +242,6 @@ class Db:
             Author:
             Louis
         '''
-        pass
     
     def add_project(self, p):
         '''
@@ -174,6 +250,12 @@ class Db:
             Author:
             Mats, Louis
         '''
+        with self.get_connection() as conn:
+            with conn.cursor() as cursor:
+                query = "INSERT INTO projekt (name, beschreibung, plätze_min, plätze_max, klasse_min, klasse_max) VALUES (%s, %s, %s, %s, %s, %s)"
+                cursor.execute(query, [p.name, p.beschreibung, p.plaetze_min, p.plaetze_max, p.klasse_min, p.klasse_max])
+                conn.commit()
+                return cursor.lastrowid
         
     def add_choice(self, uid, pid1, pid2, pid3):
         '''
@@ -182,3 +264,9 @@ class Db:
             Author:
             Max, Bendix
         '''
+        with self.get_connection() as conn:
+            with conn.cursor() as cursor:
+                for pid in (pid1, pid2, pid3):
+                    query = "INSERT INTO wählt (uid, pid) VALUES (%s, %s)"
+                    cursor.execute(query, [uid, pid])
+                conn.commit()
---
+++ usertools.py
@@ -0,0 +1,30 @@
+from functools import wraps
+from flask import g, session, request, redirect, url_for
+from flask import current_app as app
+from model import Organisator
+
+def is_logged_in():
+    return "uid" in session
+
+def is_organisator():
+    return False if not "user" in g else isinstance(g.user, Organisator)
+
+def is_schueler():
+    return False if not "user" in g else isinstance(g.user, Schueler)
+    
+def get_current_user():
+    if not is_logged_in():
+        return None
+    else:
+        if "user" not in g:
+            g.user = g.db.get_user(session["uid"])
+        
+        return g.user
+    
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if not is_logged_in():
+            return redirect(url_for('login', next=request.url))
+        return f(*args, **kwargs)
+    return decorated_function
